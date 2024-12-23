4e5f0a1f414574bb70481260c18d334eb5cce83f
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-12-23 12:46:02 -0300

    build: configure project and add openai and aws secrets manager dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "br.com.lukinhasssss"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("software.amazon.awssdk:secretsmanager:2.29.39")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
+
+    implementation("com.aallam.openai:openai-client:4.0.0-beta01")
+    implementation("io.ktor:ktor-client-core:3.0.2")
+    implementation("io.ktor:ktor-client-okhttp:3.0.2")
+
+    testImplementation(kotlin("test"))
+}
+
+// configurations.all {
+//     resolutionStrategy {
+//         failOnVersionConflict()
+//     }
+// }
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
